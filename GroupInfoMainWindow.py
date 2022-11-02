c2a31a204462a6d75f03459a509a2567c2f23cbf
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-11-02 09:06:26 +0200

    metsastys sql, GroupMainWindow

---
+++ GroupInfoMainWindow.py
@@ -0,0 +1,12 @@
+# APPLICATION FOR SHOWING SUMMARY DATA ABOUT MEAT GIVEN TO SHARE GROUP
+# ====================================================================
+
+# LIBRARIES AND MODULES 
+# ---------------------
+
+import sys
+from PyQt5 import *
+
+
+
+
